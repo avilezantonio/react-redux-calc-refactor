e6f1a0964f137e0aacb2bfa584baca507f7b730f
avilezantonio <antonio.avilez.perez@gmail.com>
2018-05-28 15:36:16 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-redux-calc-refactor
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import Calculator from "./Calculator";
+
+render(<Calculator />, document.getElementById("root"));
